4aec87e1f397b24cb7d8988710bd9a3f2573235d
Raja <rajdeepsingh6@hotmail.com>
2024-12-02 13:05:29 +0100

    fix d2p2

--- day2/calc.py
+++ day2/calc.py
@@ -1,5 +1,3 @@
-from math import trunc
-
 grid = []
 
 def readFile(filename):
@@ -13,9 +11,7 @@ def readFile(filename):
 def part1():
     sum = 0
     for line in grid:
-        # print(line)
         if is_gradual(line):
-            # print(f'\t\tis gradual')
             sum += 1
     return sum
 
@@ -35,35 +31,33 @@ def is_gradual(line):
 def part2():
     sum = 0
     for line in grid:
-        # print(line)
         if is_gradual2(line):
-#             print(f'\t\tis gradual')
             sum += 1
     return sum
 
 def is_gradual2(line):
-    num = line[0]
-    is_inc = line[0] < line[1]
-    for i in range(1, len(line)):
-        lineNum = line[i]
-        if is_inc and num > lineNum:
-#             print(f'\t\t{num} > {lineNum}')
-            arr = line[:i] + line[i+1:]
-            return is_gradual(arr)
-        elif not is_inc and num < lineNum:
-#             print(f'\t\t{num} < {lineNum}')
-            arr = line[:i] + line[i+1:]
-            return is_gradual(arr)
-        if 4 <= abs(num - lineNum) or abs(num - lineNum) <= 0:
-#             print(f'\t\t{num} - {lineNum} = {abs(num - lineNum)}')
-            arr = line[:i] + line[i+1:]
-            return is_gradual(arr)
-        num = lineNum
-    return 1
+    if is_gradual(line):
+        return 1
+    for i in range(len(line)):
+        if is_gradual(line[:i] + line[i + 1:]):
+            return 1
+    return 0
+    # below code does not work
+    # num = line[0]
+    # is_inc = line[0] < line[1]
+    # for i in range(1, len(line)):
+    #     lineNum = line[i]
+    #     if is_inc and num > lineNum:
+    #         return is_gradual(line[:i] + line[i + 1:]) or is_gradual(line[:i - 1] + line[i:])
+    #     elif not is_inc and num < lineNum:
+    #         return is_gradual(line[:i] + line[i + 1:]) or is_gradual(line[:i - 1] + line[i:])
+    #     if 4 <= abs(num - lineNum) or abs(num - lineNum) <= 0:
+    #         return is_gradual(line[:i] + line[i + 1:]) or is_gradual(line[:i - 1] + line[i:])
+    #     num = lineNum
+    # return 1
 
 if __name__ == "__main__":
     readFile('input.txt')
-    print(f'part1 is {part1()}')
-    # TODO
-    print(f'part2 is {part2()} (436 is too low)')
+    print(f'part1 is {part1()} (402 is correct)')
+    print(f'part2 is {part2()} (455 is correct)')
 
