42a42368f79635ad1de9e95e4fcef0451613d673
Raja <rajdeepsingh6@hotmail.com>
2024-12-03 20:36:09 +0100

    shorter day1

--- day1/calc.py
+++ day1/calc.py
@@ -2,39 +2,22 @@
 numbers = []
 
 def readFile(filename):
-    file = open(filename, 'r')
-    lines = file.readlines()
-    for line in lines:
-        line = line.strip()
-        numbers.append(line.split())
+    lines = open(filename, 'r').readlines()
+    [numbers.append(line.strip().split()) for line in lines]
 
 def part1():
-    list1 = []
-    list2 = []
-    for number in numbers:
-        list1.append(number[0])
-        list2.append(number[1])
+    list1 = [number[0] for number in numbers]
+    list2 = [number[1] for number in numbers]
     list1.sort()
     list2.sort()
-    som = 0
-    for i in range(len(list1)):
-        verschil = abs(int(list1[i]) - int(list2[i]))
-        som = som + verschil
-    return som
+    return sum([abs(int(list1[i]) - int(list2[i])) for i in range(len(list1))])
 
 def part2():
-    list1 = []
-    list2 = []
-    for number in numbers:
-        list1.append(number[0])
-        list2.append(number[1])
-    sum = 0
-    for l1 in list1:
-        sum += list2.count(l1) * int(l1)
-    return sum
+    list1 = [number[0] for number in numbers]
+    list2 = [number[1] for number in numbers]
+    return sum([int(l1) * list2.count(l1) for l1 in list1])
 
 if __name__ == "__main__":
     readFile('input.txt')
-    print(f'part1 is {part1()}')
-    print(f'part2 is {part2()}')
-
+    print(f'part1 is {part1()} (1189304 is correct)')
+    print(f'part2 is {part2()} (24349736 is correct)')
