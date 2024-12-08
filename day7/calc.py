6256a6133a67681b084514b3c0ae27f84d605a95
Raja <rajdeepsingh6@hotmail.com>
2024-12-08 14:33:14 +0100

    solved day7p2

--- day7/calc.py
+++ day7/calc.py
@@ -10,7 +10,6 @@ def readFile(filename):
             'numbers': list(map(int, sections[1].split(' ')))
         }
         equs.append(equ)
-    print(equs)
 
 def check_test(test, total, numbers):
     if total > test:
@@ -19,18 +18,22 @@ def check_test(test, total, numbers):
         return total + numbers[0] == test or total * numbers[0] == test
     return check_test(test, total + numbers[0], numbers[1:]) or check_test(test, total * numbers[0], numbers[1:])
 
+def check_test2(test, total, numbers):
+    if total > test:
+        return False
+    if len(numbers) == 0:
+        return total == test
+    return (check_test2(test, total + numbers[0], numbers[1:]) or
+         check_test2(test, total * numbers[0], numbers[1:]) or
+         check_test2(test, int(str(total) + str(numbers[0])), numbers[1:]))
 
 def part1():
-    total = 0
-    for equ in equs:
-        if check_test(equ['test'], 0, equ['numbers']):
-            total += equ['test']
-    return total
+    return sum([equ['test'] for equ in equs if check_test(equ['test'], 0, equ['numbers'])])
 
 def part2():
-    return 0
+    return sum([equ['test'] for equ in equs if check_test2(equ['test'], equ['numbers'][0], equ['numbers'][1:])])
 
 if __name__ == "__main__":
     readFile('input.txt')
-    print(f'part1 is {part1()} (3598800864292 is correct)')
-    print(f'part2 is {part2()} (... is correct)')
+    print(f'part1 is\n\t {part1()}\n\t(3598800864292 is correct)')
+    print(f'part2 is\n\t {part2()}\n\t(340362529351427 is correct)')
